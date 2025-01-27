1eb5d81e21552ecd1d4e61786a46e9935efc1c18
santiago861 <santiago_reyes@ciencias.unam.mx>
2025-01-26 21:23:14 -0600

    #4 solved

---
+++ Array/4-MedianOfTwoSortedArrays.py
@@ -0,0 +1,8 @@
+import math
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        sortedArr = sorted(nums1 + nums2)
+        if len(sortedArr) % 2 == 1:
+            return sortedArr[int(math.floor(len(sortedArr) / 2))]
+        else:
+            return (sortedArr[int(len(sortedArr) / 2)] + sortedArr[int((len(sortedArr) / 2) - 1)]) / 2
